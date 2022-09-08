db49fe1f45a6f5d8b6e52e7361caa2e398b44fcf
RonSG <gonzalo2196@icloud.com>
2022-09-08 11:07:13 -0500

    Configuracion de tailwinds V3.1.8, el config con imagenes, bg, textcolors, y font para cualquier proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,9 +19,21 @@ module.exports = {
 'europe': "url('/public/img/europe.jpg')",
 'iceland': "url('/public/img/iceland.jpg')",
       },
-      backgroundColor: {
+      backgroundColor: theme=> ({
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary':'#61AEC9',
         
+      }),
+      textColor: {
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary':'#61AEC9',
       },
+      fontFamily: {
+        Montserrat:['Montserrat', 'sans-serif']
+      }
     },
   },
   plugins: [],
